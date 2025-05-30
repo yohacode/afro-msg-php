b3c1d3d35be7cad20e97ea355f311e12f4168809
yohacode <johnpro3269@gmail.com>
2025-05-30 15:58:34 +0300

    AppContainerInterfaceUpdate

--- src/core/AppContainer.php
+++ src/core/AppContainer.php
@@ -11,17 +11,110 @@ use App\Contracts\AppContainerInterface;
 
 class AppContainer implements AppContainerInterface
 {
-    /**
-     * Properties store instance and datas
-     */
+   /**
+    * Properties store instance and datas
+    */
 
-     public function getInstances(): void
-     {
-        // 
-     }
+   public function getInstances(): void
+   {
+      // 
+   }
 
-     public function getSerialize(): void
-     {
-        // 
-     }
+   public function getSerialize(): void
+   {
+      // 
+   }
+
+   public function getDeserialize(): void
+   {
+      // 
+   }
+   public function getData(): void
+   {
+      // 
+   }
+   public function getDataByKey(string $key)
+   {
+      // 
+   }
+   public function setData(string $key, $value): void
+   {
+      // 
+   }
+   public function setInstances(string $key ,$value): void
+   {
+      // 
+   }
+   public function getInstancesByKey(string $key)
+   {
+      // 
+   }
+   public function getInstancesByClass(string $class)
+   {
+      // 
+   }
+   public function getInstancesByClassName(string $className)
+   {
+      // 
+   }
+   public function getInstancesByClassNameAndKey(string $className, string $key)
+   {
+      // 
+   }
+   public function getSerializeData(): void
+   {
+      // 
+   }
+   public function getDeserializeData(): void
+   {
+      // 
+   }
+   public function getSerializeDataArray(): void
+   {
+      // 
+   }
+   public function getDeserializeDataArray(): void
+   {
+      // 
+   }
+   public function getSerializeDataObject(): void
+   {
+      // 
+   }
+   public function getDeserializeDataObject(): void
+   {
+      // 
+   }
+   public function getSerializeDataObjectArray(): void
+   {
+      // 
+   }
+   public function getDeserializeDataObjectArray(): void
+   {
+      // 
+   }
+   public function getSerializeDataObjectArrayWithKey(): void
+   {
+      // 
+   }
+   public function getDeserializeDataObjectArrayWithKey(): void
+   {
+      // 
+   }
+   public function getSerializeDataObjectArrayWithKeyAndValue(): void
+   {
+      // 
+   }
+   public function getSecurity(): void
+   {
+      // 
+   }
+   public function getSecurityData(): void
+   {
+      // 
+   }
+   public function getSecurityDataArray(): void
+   {
+      // 
+   }
 }
